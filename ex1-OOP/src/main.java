f1f5ed4a07b3002bcb2deb04b2a8dc7c821e9cf6
Rafael Magalhaes <167685811+rafamagsINTL@users.noreply.github.com>
2025-08-05 13:18:42 -0300

    Add files via upload
    ex1 incompleto

---
+++ ex1-OOP/src/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        if(NPA >= 60){
+            System.out.println("passou");
+        }else{
+            System.out.println("nao passou");
+        }
+
+        scanner.close();
+    }
+}
